2fd1ada1cf5c63f9a40a3b066ee1328e9f0707a2
TheEternalAce <63331137+TheEternalAce@users.noreply.github.com>
2023-03-25 01:08:53 -0400

    comment out entire config file

--- ElementsServerConfig.cs
+++ ElementsServerConfig.cs
@@ -1,18 +1,18 @@
-﻿using System.ComponentModel;
-using Terraria.ModLoader.Config;
+﻿//using System.ComponentModel;
+//using Terraria.ModLoader.Config;
 
-namespace ShardsOfAtheria.Config
-{
-    [BackgroundColor(164, 153, 190)]
-    [Label("$Mods.MMZeroElements.Config.ServerLabel")]
-    public class ElementsServerConfig : ModConfig
-    {
-        public override ConfigScope Mode => ConfigScope.ServerSide;
+//namespace ShardsOfAtheria.Config
+//{
+//    [BackgroundColor(164, 153, 190)]
+//    [Label("$Mods.MMZeroElements.Config.ServerLabel")]
+//    public class ElementsServerConfig : ModConfig
+//    {
+//        public override ConfigScope Mode => ConfigScope.ServerSide;
 
-        [Label("$Mods.MMZeroElements.Config.LegacySystem")]
-        [Tooltip("$Mods.MMZeroElements.ConfigDesc.LegacySystem")]
-        [DefaultValue(false)]
-        [ReloadRequired]
-        public bool legacySystem;
-    }
-}
+//        [Label("$Mods.MMZeroElements.Config.LegacySystem")]
+//        [Tooltip("$Mods.MMZeroElements.ConfigDesc.LegacySystem")]
+//        [DefaultValue(false)]
+//        [ReloadRequired]
+//        public bool legacySystem;
+//    }
+//}

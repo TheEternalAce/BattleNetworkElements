61fdccba8aa075167688a95ef21601de6f886296
TheEternalAce <63331137+TheEternalAce@users.noreply.github.com>
2023-03-25 01:01:14 -0400

    prepare for upcoming Wood Element

---
+++ ElementsServerConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using Terraria.ModLoader.Config;
+
+namespace ShardsOfAtheria.Config
+{
+    [BackgroundColor(164, 153, 190)]
+    [Label("$Mods.MMZeroElements.Config.ServerLabel")]
+    public class ElementsServerConfig : ModConfig
+    {
+        public override ConfigScope Mode => ConfigScope.ServerSide;
+
+        [Label("$Mods.MMZeroElements.Config.LegacySystem")]
+        [Tooltip("$Mods.MMZeroElements.ConfigDesc.LegacySystem")]
+        [DefaultValue(false)]
+        [ReloadRequired]
+        public bool legacySystem;
+    }
+}
